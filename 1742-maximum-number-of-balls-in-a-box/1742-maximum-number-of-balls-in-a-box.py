dc836d85e79c374b7541bea86ada7ea73cea370a
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2022-08-07 17:50:25 +0530

    Time: 1267 ms (19.79%), Space: 14 MB (30.42%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -1,14 +1,18 @@
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
         p=dict()
-        for i in range(highLimit+1):
-            p[i]=0
         for i in range(lowLimit,highLimit+1):
             if i<=9:
-                p[i]+=1
+                if i not in p:
+                    p[i]=1
+                else:
+                    p[i]+=1
             else:
                 c=0
                 for j in str(i):
                     c+=int(j)
-                p[c]+=1
+                if c not in p:
+                    p[c]=1
+                else:
+                    p[c]+=1
         return max(p.values())
