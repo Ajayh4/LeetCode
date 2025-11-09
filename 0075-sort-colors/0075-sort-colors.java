b8bfec62900c98503a751d81d36feb20cb2013d0
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2025-11-09 08:35:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (6.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int a=0,b=0,c=0;
-        for(int i: nums)
+        int n=nums.length,i=0,j=n-1;
+        while(i<j)
         {
-            if(i==0)
-            a+=1;
-            else if(i==1)
-            b+=1;
-            else
-            c+=1;
+            while(i<n && nums[i]==0)
+            i+=1;
+            while(j>i && nums[j]!=0)
+            j-=1;
+            if(i<j)
+            {
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+                i+=1;
+                j-=1;
+            }
         }
-        int i=0;
-        for(int j=0;j<a;j++)
-        nums[i++]=0;
-        for(int j=0;j<b;j++)
-        nums[i++]=1;
-        for(int j=0;j<c;j++)
-        nums[i++]=2;
+        // System.out.println(Arrays.toString(nums));
+        i=0;
+        j=n-1;
+        while(i<n && nums[i]==0)
+        i+=1;
+        // System.out.println(i+" "+j);
+        while(i<j)
+        {
+            while(i<n && nums[i]==1)
+            i+=1;
+            while(j>i && nums[j]!=1)
+            j-=1;
+            if(i<j)
+            {
+                // System.out.println(i+" "+j);
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+                i+=1;
+                j-=1;
+            }
+        }
     }
 }
