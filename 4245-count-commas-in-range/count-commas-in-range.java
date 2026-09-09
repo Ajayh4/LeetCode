d8e61588c8c406df5e9f1922cd954f22adc36f72
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-09-09 19:28:43 +0530

    Time: 1 ms (83.58%) | Memory: 42.3 MB (85.34%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,12 +1,17 @@
 class Solution {
     public int countCommas(int n) {
+        if(n<1000)
+        return 0;
         int ans=0;
-        for(int i=1000;i<=n;i++)
+        int tmp=100;
+        while(tmp*10<=n)
         {
-            int count=(int)Math.log10(n)+1;
-            if(count>=4)
-            ans+=(count-1)/3;
+            tmp*=10;
+            int count=Math.min((tmp*10-1),n)-tmp+1;
+            int length=(int)Math.log10(tmp)+1;
+            ans+=((length-1)/3*count);
         }
+        
         return ans;
     }
 }
