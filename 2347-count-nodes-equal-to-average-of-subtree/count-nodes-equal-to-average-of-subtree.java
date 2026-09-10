e6637c52989f01b3cd2bc3f9317b18698451a1c2
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-09-10 16:37:19 +0530

    Time: 1 ms (56.32%) | Memory: 45.7 MB (37.15%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -16,27 +16,27 @@
 class Solution {
     static class Pair
     {
-        int cnt,sum;
-        Pair(int cnt,int sum)
+        int cnt,sum,ans;
+        Pair(int cnt,int sum,int ans)
         {
             this.cnt=cnt;
             this.sum=sum;
+            this.ans=ans;
         }
     }
     static int ans=0;
     static Pair rec(TreeNode root)
     {
         if(root==null)
-        return new Pair(0,0);
+        return new Pair(0,0,0);
         Pair ls=rec(root.left),rs=rec(root.right);
         int tcnt=ls.cnt+rs.cnt+1,tsum=root.val+ls.sum+rs.sum;
+        boolean flag=false;
         if((tsum/tcnt)==root.val)
-        ans+=1;
-        return new Pair(tcnt,tsum);
+        flag=true;
+        return new Pair(tcnt,tsum,(flag)?(ls.ans+rs.ans+1):(ls.ans+rs.ans));
     }
     public int averageOfSubtree(TreeNode root) {
-        ans=0;
-        rec(root);
-        return ans;
+        return rec(root).ans;
     }
 }
